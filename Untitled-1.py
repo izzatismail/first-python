c70fbf29261446495cd4e0be09af8aa98680ff73
Izzat Ismail <mizzat1996@gmail.com>
2023-01-02 13:37:13 +0800

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+print("Hello World! My First Python")
+
+# To define function in Python, use def, then indent the rest of the code
+def get_choices():
+    player_choice = "Rock"
+    computer_choice = "Paper"
+
+    return computer_choice
