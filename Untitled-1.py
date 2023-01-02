0d335596f2d624218559e0e3981c9031db17004e
Izzat Ismail <mizzat1996@gmail.com>
2023-01-02 16:55:40 +0800

    Learnt how to add user input, dictionary

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,7 +2,13 @@ print("Hello World! My First Python")
 
 # To define function in Python, use def, then indent the rest of the code
 def get_choices():
-    player_choice = "Rock"
+    # To get input from user
+    player_choice = input("Enter a choice (rock, paper, scissors): ") 
     computer_choice = "Paper"
+    
+     # Dictionary is similar to HashMap in Java
+    choices_dictionary = {"Player": player_choice, "Computer": computer_choice}
+    return choices_dictionary
 
-    return computer_choice
+choices = get_choices()
+print(choices)
